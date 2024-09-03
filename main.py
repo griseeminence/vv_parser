ac40db29a6294d52a43c06cb1accc5f4bde704be
griseeminence <p.kolt080@gmail.com>
2024-09-03 18:01:18 +0200

    new cycle for callecting all links

--- main.py
+++ main.py
@@ -1,46 +1,49 @@
 import os
 
-from bs4 import BeautifulSoup
 import requests
 import json
 import time
 import random
-
-headers2 = {
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36"
-}
+from bs4 import BeautifulSoup
 
 headers = {
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36"
 }
 
-# url = "https://vkusvill.ru/media/recipes/"
-# req = requests.get(url, headers=headers)
-# src = req.text
-
-# with open(f'data/first_page.html', "w", encoding='utf-8') as file:
-#     file.write(src)
-
-with open("data/first_page.html", encoding='utf-8') as file:
-    src = file.read()
-
-soup = BeautifulSoup(src, "lxml")
-recipes_links = soup.find_all(class_='VV_NewsCard__Layout')
-
 recipes_dict = {}
+PAGE_NUMBER = 66
 
-for i in recipes_links:
-    i_text = i.get('aria-label').replace('\xa0', ' ')
-    i_href = i.get('href')
-    recipes_dict[i_text] = 'https://vkusvill.ru/' + i_href
+for page in range(1, 5):
+    url_pages = f'https://vkusvill.ru/media/recipes/?utm_source=site&utm_medium=main&utm_campaign=content&PAGEN_1={page}'
+    req = requests.get(url_pages, headers=headers)
+    folder_name = f"data/recipe_pages/"
 
-for i,y in recipes_dict.items():
-    print(y)
+    if os.path.exists(folder_name):
+        print("Папка уже существует!")
+    else:
+        os.mkdir(folder_name)
+
+    with open(f'data/recipe_pages/page_{page}.html', 'w', encoding='utf-8') as file:
+        file.write(req.text)
+    with open(f'data/recipe_pages/page_{page}.html', encoding='utf-8') as file:
+        src = file.read()
+
+    soup = BeautifulSoup(src, "lxml")
+    recipes_links = soup.find_all(class_='VV_NewsCard__Layout')
+
+    for i in recipes_links:
+        i_text = i.get('aria-label').replace('\xa0', ' ')
+        i_href = i.get('href')
+        recipes_dict[i_text] = 'https://vkusvill.ru/' + i_href
+
+    time.sleep(random.randrange(2, 4))
+
+with open("data/recipe_pages/result_recipe_pages.json", "a", encoding="utf-8") as file:
+    json.dump(recipes_dict, file, indent=4, ensure_ascii=False)
 
 # Список активных ссылок на рецепты с первой страницы
-full_links = [('https://vkusvill.ru/' + link) for link in recipes_dict.values()]
+# full_links = [('https://vkusvill.ru/' + link) for link in recipes_dict.values()]
 
 # for i in recipes_dict.values():
 #     full_links.append('https://vkusvill.ru/'+i)
@@ -56,29 +59,35 @@ full_links = [('https://vkusvill.ru/' + link) for link in recipes_dict.values()]
 # with open(f'data/first_recipe.html', encoding='utf-8') as file:
 #     src = file.read()
 print(recipes_dict)
-full_links2 =['https://vkusvill.ru/media/recipes/pryanaya-dynya-s-glintveynom.html']
+
+# Для теста
+# full_links2 = ['https://vkusvill.ru/media/recipes/pryanaya-dynya-s-glintveynom.html']
+
+
 result = []
 for title, link in recipes_dict.items():
     req = requests.get(link, headers=headers)
 
+    folder_name = f"data/recipes/"
 
-    with open(f"data/recipe_{title.replace(' ', '_')}.html", "w", encoding='utf-8') as file:
+    if os.path.exists(folder_name):
+        print("Папка уже существует!")
+    else:
+        os.mkdir(folder_name)
+
+    with open(f'{folder_name}_{title.replace(" ", "_")}.html', 'w', encoding='utf-8') as file:
         file.write(req.text)
 
-    with open(f"data/recipe_{title.replace(' ', '_')}.html", encoding='utf-8') as file:
+    with open(f'{folder_name}_{title.replace(" ", "_")}.html', encoding='utf-8') as file:
         src = file.read()
 
-
-
-    print(link)
-    print(title)
     recipe_number = 1
     # Сохранение HTML в переменную вместо записи в файл
     # src2 = req.text
 
     # Парсинг HTML с BeautifulSoup
     soup = BeautifulSoup(src, "lxml")
-    title = soup.find('title')
+    # title = soup.find('title')
     ingredients_name_list = soup.find_all(class_='VV_RecipeDetailIng__Col _name')
     ingredients_value_list = soup.find_all(class_='VV_RecipeDetailIng__Col _val b500')
     ingredients_name_strip = [ingredient.text.strip() for ingredient in ingredients_name_list]
@@ -101,7 +110,6 @@ for title, link in recipes_dict.items():
     time.sleep(random.randrange(3, 6))
     print(f'Рецепт {recipe_number} обработан')
     recipe_number += 1
-
 
 with open("data/result_data.json", "a", encoding="utf-8") as file:
     json.dump(result, file, indent=4, ensure_ascii=False)
