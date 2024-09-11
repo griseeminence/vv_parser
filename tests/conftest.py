9b165c78a9b2c09f461570ded3b8bad64e1ac022
griseeminence <p.kolt080@gmail.com>
2024-09-11 10:31:46 +0200

    add more fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+import json
 import os
 import shutil
 
@@ -9,7 +10,6 @@ import unittest.mock as mock
 @pytest.fixture(autouse=True)
 def mock_request():
     with mock.patch('requests.get') as mock_request:
-        # Настроим мок-объект
         mock_request.status_code = 200
         mock_request.return_value.text = (
             '<html><body>'
@@ -23,6 +23,7 @@ def mock_request():
         )
         yield mock_request
 
+
 @pytest.fixture(autouse=True)
 def setup_and_teardown():
     # Создаем папку data перед выполнением тестов
@@ -32,3 +33,71 @@ def setup_and_teardown():
     # Удаляем папку data после выполнения тестов
     if os.path.exists('data'):
         shutil.rmtree('data')
+
+
+@pytest.fixture()
+def parse_recipes_expected():
+    expected = [
+        {
+            'Пряная дыня с глинтвейном': {
+                'Steps': [
+                    'Налейте вино в кастрюлю.'
+                ],
+                'Ingredients': {
+                    'дыня плотная': '1 шт. (примерно 500 г)'
+                }
+            }
+        }
+    ]
+    return expected
+
+
+@pytest.fixture()
+def parse_recipes_expected_json():
+    expected_json = json.dumps(
+        [
+            {
+                'Пряная дыня с глинтвейном': {
+                    'Steps': [
+                        'Налейте вино в кастрюлю.'
+                    ],
+                    'Ingredients': {
+                        'дыня плотная': '1 шт. (примерно 500 г)'
+                    }
+                }
+            }
+        ],
+        indent=4,
+        ensure_ascii=False
+    )
+    return expected_json
+
+
+@pytest.fixture()
+def collect_links_expected():
+    expected = {
+        'Пряная дыня с глинтвейном': (
+            'https://vkusvill.ru//media/recipes/pryanaya-dynya-s-glintveynom.html'
+        )
+    }
+    return expected
+
+
+@pytest.fixture()
+def collect_links_expected_json():
+    expected_json = json.dumps(
+        {
+            'Пряная дыня с глинтвейном': (
+                'https://vkusvill.ru//media/recipes/pryanaya-dynya-s-glintveynom.html'
+            )
+        },
+        indent=4,
+        ensure_ascii=False
+    )
+    return expected_json
+
+
+@pytest.fixture()
+def recipes_dict_example():
+    recipes_dict = Parser.collect_links(1, 2)
+    return recipes_dict
--- tests/parser_tests.py
+++ tests/parser_tests.py
@@ -1,27 +1,23 @@
 import pytest
-# from unittest.mock import patch, mock_open
-import unittest.mock as mock
-import os
 import json
-
-import requests
+import unittest.mock as mock
 
 from main import Parser
 
 
-# TODO: определить в класс, вывести doc строку в коммент к классу (про autouse)
-def test_correct_request_bs4():
+# TODO: Определить в класс, вывести doc строку в коммент к классу (про autouse)
+# TODO: Проверить фикстуры, убрать дубли кода
+# TODO: Убрать принты
+
+def test_correct_request_bs4(collect_links_expected):
     """
     Autouse pytest.fixture "mock request" for request.text and request.status_code
     Autouse pytest.fixture "setup_and_teardown" for delete data directory after tests
     """
     result = Parser.collect_links(1, 2)
-    expected = {
-        'Пряная дыня с глинтвейном': (
-            'https://vkusvill.ru//media/recipes/pryanaya-dynya-s-glintveynom.html'
-        )
-    }
+    expected = collect_links_expected
     print(f"Результат: {result}")
+    print(f"expected: {expected}")
     assert result == expected
 
 
@@ -31,8 +27,6 @@ def test_correct_request_bs4():
 def test_directory_creation(mock_open, mock_mkdir, mock_exists):
     mock_exists.return_value = False
     Parser.collect_links(start_page=1, end_page=2)
-    call = mock_open.call_args_list
-    print(call)
     mock_mkdir.assert_any_call('data/recipe_pages/')
     mock_open.assert_any_call('data/recipe_pages/result_recipe_pages.json', 'a', encoding='utf-8')
     mock_open.assert_any_call('data/recipe_pages/page_1.html', 'w', encoding='utf-8')
@@ -40,90 +34,50 @@ def test_directory_creation(mock_open, mock_mkdir, mock_exists):
     assert mock_open.call_count == 3
 
 
-def test_collect_links_creates_json():
+def test_collect_links_creates_json(collect_links_expected_json):
     result = Parser.collect_links(1, 2)
     result_json = json.dumps(result, indent=4, ensure_ascii=False)
-    expected_json = json.dumps(
-        {
-            'Пряная дыня с глинтвейном': (
-                'https://vkusvill.ru//media/recipes/pryanaya-dynya-s-glintveynom.html'
-            )
-        },
-        indent=4,
-        ensure_ascii=False
-    )
+    expected_json = collect_links_expected_json
     assert expected_json == result_json
     print(f'Expected JSON: {expected_json}')
     print(f'Result JSON: {result_json}')
 
 
-def test_parse_recipes():
+def test_parse_recipes(parse_recipes_expected):
     recipes_dict = Parser.collect_links(1, 2)
-    print(recipes_dict)
+    print(f'recipes_dict: {recipes_dict}')
     result = Parser.parse_recipes(recipes_dict)
-    print(result)
-    expected = [
-        {
-            'Пряная дыня с глинтвейном': {
-                'Steps': [
-                    'Налейте вино в кастрюлю.'
-                ],
-                'Ingredients': {
-                    'дыня плотная': '1 шт. (примерно 500 г)'
-                }
-            }
-        }
-    ]
+    print(f'result: {result}')
+    expected = parse_recipes_expected
+    print(f'expected: {expected}')
     assert expected == result
 
 
-def test_parse_recipes_json(mock_request):
+def test_parse_recipes_json(mock_request, parse_recipes_expected_json):
     recipes_dict = Parser.collect_links(1, 2)
     result = Parser.parse_recipes(recipes_dict)
     result_json = json.dumps(result, indent=4, ensure_ascii=False)
-    print(result_json)
-    expected_json = json.dumps(
-        [
-            {
-                'Пряная дыня с глинтвейном': {
-                    'Steps': [
-                        'Налейте вино в кастрюлю.'
-                    ],
-                    'Ingredients': {
-                        'дыня плотная': '1 шт. (примерно 500 г)'
-                    }
-                }
-            }
-        ],
-        indent=4,
-        ensure_ascii=False
-    )
+    print(f'result_json: {result_json}')
+    expected_json = parse_recipes_expected_json
+    print(f'expected_json: {expected_json}')
     assert expected_json == result_json
 
 
 @mock.patch('os.path.exists')
 @mock.patch('os.mkdir')
 @mock.patch('builtins.open', new_callable=mock.mock_open)
-def test_parser_recipes_directory_creation(mock_open, mock_mkdir, mock_exists):
+def test_parser_recipes_directory_creation(mock_open, mock_mkdir, mock_exists, recipes_dict_example):
     mock_exists.return_value = False
-    recipes_dict = {
-        'Пряная дыня с глинтвейном': 'https://vkusvill.ru//media/recipes/pryanaya-dynya-s-glintveynom.html'
-    }
-
-    # Запуск парсера
+    recipes_dict = recipes_dict_example
     Parser.parse_recipes(recipes_dict)
-
-    # Проверка, что каталог был создан
     mock_mkdir.assert_called_once_with('data/recipes/')
-
-    # Проверка, что файл для HTML был открыт трижды (для записи и для чтения) + JSON
-    assert mock_open.call_count == 3  # Добавляем проверку на 3 вызова open
-
-    # Проверка вызова для записи HTML-файла
     mock_open.assert_any_call('data/recipes/_Пряная_дыня_с_глинтвейном.html', 'w', encoding='utf-8')
-
-    # Проверка вызова для чтения HTML-файла
     mock_open.assert_any_call('data/recipes/_Пряная_дыня_с_глинтвейном.html', encoding='utf-8')
-
-    # Проверка вызова для записи JSON-файла
     mock_open.assert_any_call('data/result_data.json', 'a', encoding='utf-8')
+    assert mock_open.call_count == 3
+
+
+def test_start_parser(parse_recipes_expected):
+    result = Parser.start_parser(1, 2)
+    expected = parse_recipes_expected
+    assert expected == result
